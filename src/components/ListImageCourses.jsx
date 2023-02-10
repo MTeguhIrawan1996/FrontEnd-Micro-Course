83991a8ae0b73bb300a2c6ab33816cae54be2f55
MTeguhirawan1996 <teguhirawan1996@gmail.com>
2023-02-10 11:26:09 +0700

    Revisi Rounded

--- src/components/ListImageCourses.jsx
+++ src/components/ListImageCourses.jsx
@@ -22,7 +22,7 @@ const ListImageCourses = () => {
               priority
               sizes="max-w-full"
             />
-            <div className="absolute opacity-0 hover:opacity-100 bg-primary-hover/10 inset-0 backdrop-blur-sm transition-all ease-linear duration-300 delay-0 cursor-pointer flex justify-center items-center w-full group">
+            <div className="absolute opacity-0 hover:opacity-100 rounded-lg bg-primary-hover/10 inset-0 backdrop-blur-sm transition-all ease-linear duration-300 delay-0 cursor-pointer flex justify-center items-center w-full group">
               <div className="w-[20%] aspect-square rounded-full opacity-0 bg-orange-400 flex justify-center items-center group-hover:opacity-100 transition-opacity ease-linear duration-[0.5s] delay-0">
                 <Link href={`/courses/${i + 1}`} className="w-full">
                   <IconPreview className="w-[80%] h-[80%] mx-auto text-transparent" />
