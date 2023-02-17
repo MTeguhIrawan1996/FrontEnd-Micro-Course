31bf800dea3e6730305619a94907fd6b87374ce7
MTeguhirawan1996 <teguhirawan1996@gmail.com>
2023-02-17 18:00:56 +0700

    Accordion Rev

--- src/pages/dashboard/[cid].jsx
+++ src/pages/dashboard/[cid].jsx
@@ -2,20 +2,27 @@ import React, { useState } from "react";
 import { Button, Layout, TitleText, TypingText } from "../../components";
 import styles from "../../styles";
 import dynamic from "next/dynamic";
-import {
-  Accordion,
-  AccordionHeader,
-  AccordionBody,
-} from "@material-tailwind/react";
+import { Accordion, AccordionBody } from "@material-tailwind/react";
 import { ChevronDownIcon } from "@heroicons/react/24/solid";
+import { learnList } from "../../constants";
+import IconPlay from "../../../public/images/icon-play.svg";
+import IconLock from "../../../public/images/icon-lock.svg";
 
 const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
 
 const DetailMyCourses = () => {
-  const [open, setOpen] = useState(0);
+  const [keys, setKeys] = useState([{ key: 0 }]);
 
-  const handleOpen = (value) => {
-    setOpen(open === value ? 0 : value);
+  const handleClick = (key) => {
+    setKeys((prevKeys) => {
+      const filteredKeys = prevKeys.filter((k) => k.key !== key);
+      const updatedKeys = [...prevKeys, { key: key }];
+      return prevKeys.some((k) => k.key === key) ? filteredKeys : updatedKeys;
+    });
+  };
+
+  const open = (i) => {
+    return keys?.some((k) => k.key === i);
   };
   return (
     <section className={`${styles.paddings} max-h-screen overflow-y-auto`}>
@@ -53,163 +60,50 @@ const DetailMyCourses = () => {
               <Button text="Next" styles="px-12 rounded-xl drop-shadow-none" />
             </div>
           </div>
-          <div className="hide-scrollbar flex max-h-[50vh] flex-col items-start justify-start gap-2 overflow-hidden overflow-y-auto bg-yellow-400">
-            <Accordion
-              open={open === 1}
-              className="w-full rounded-lg bg-zinc-200 px-4 py-6 text-sm font-normal md:w-[70%] lg:w-[50%]"
-            >
-              <div
-                onClick={() => handleOpen(1)}
-                className={`group flex w-full max-w-full cursor-pointer items-center justify-between`}
+          <div className="hide-scrollbar flex max-h-[50vh] w-full flex-col items-start justify-start gap-2 overflow-hidden overflow-y-auto bg-yellow-400">
+            {learnList.map((data, i) => (
+              <Accordion
+                open={open(i)}
+                className="w-full rounded-lg bg-zinc-200 px-4 py-6 text-sm font-normal md:w-[70%] lg:w-[50%]"
+                key={`${data.title}-${i}`}
               >
-                <span className="font-normal text-secondary-black first-letter:uppercase">
-                  tes
-                </span>
-                <ChevronDownIcon
-                  className={`h-4 w-4 text-zinc-700 group-hover:text-zinc-400 ${
-                    open === 1 && "rotate-180"
-                  }`}
-                />
-              </div>
-              <AccordionBody>
-                We&apos;re not always in the position that we want to be at.
-                We&apos;re constantly growing. We&apos;re constantly making
-                mistakes. We&apos;re constantly trying to express ourselves and
-                mistakes. We&apos;re constantly trying to express ourselves and
-                mistakes. We&apos;re constantly trying to express ourselves and
-                actualize our dreams.
-              </AccordionBody>
-            </Accordion>
-            <Accordion
-              open={open === 1}
-              className="w-full rounded-lg bg-zinc-200 px-4 py-6 text-sm font-normal md:w-[70%] lg:w-[50%]"
-            >
-              <div
-                onClick={() => handleOpen(1)}
-                className={`group flex w-full max-w-full cursor-pointer items-center justify-between`}
-              >
-                <span className="font-normal text-secondary-black first-letter:uppercase">
-                  tes
-                </span>
-                <ChevronDownIcon
-                  className={`h-4 w-4 text-zinc-700 group-hover:text-zinc-400 ${
-                    open === 1 && "rotate-180"
-                  }`}
-                />
-              </div>
-              <AccordionBody>
-                We&apos;re not always in the position that we want to be at.
-                We&apos;re constantly growing. We&apos;re constantly making
-                mistakes. We&apos;re constantly trying to express ourselves and
-                mistakes. We&apos;re constantly trying to express ourselves and
-                mistakes. We&apos;re constantly trying to express ourselves and
-                actualize our dreams.
-              </AccordionBody>
-            </Accordion>
-            <Accordion
-              open={open === 1}
-              className="w-full rounded-lg bg-zinc-200 px-4 py-6 text-sm font-normal md:w-[70%] lg:w-[50%]"
-            >
-              <div
-                onClick={() => handleOpen(1)}
-                className={`group flex w-full max-w-full cursor-pointer items-center justify-between`}
-              >
-                <span className="font-normal text-secondary-black first-letter:uppercase">
-                  tes
-                </span>
-                <ChevronDownIcon
-                  className={`h-4 w-4 text-zinc-700 group-hover:text-zinc-400 ${
-                    open === 1 && "rotate-180"
-                  }`}
-                />
-              </div>
-              <AccordionBody>
-                We&apos;re not always in the position that we want to be at.
-                We&apos;re constantly growing. We&apos;re constantly making
-                mistakes. We&apos;re constantly trying to express ourselves and
-                mistakes. We&apos;re constantly trying to express ourselves and
-                mistakes. We&apos;re constantly trying to express ourselves and
-                actualize our dreams.
-              </AccordionBody>
-            </Accordion>
-            <Accordion
-              open={open === 1}
-              className="w-full rounded-lg bg-zinc-200 px-4 py-6 text-sm font-normal md:w-[70%] lg:w-[50%]"
-            >
-              <div
-                onClick={() => handleOpen(1)}
-                className={`group flex w-full max-w-full cursor-pointer items-center justify-between`}
-              >
-                <span className="font-normal text-secondary-black first-letter:uppercase">
-                  tes
-                </span>
-                <ChevronDownIcon
-                  className={`h-4 w-4 text-zinc-700 group-hover:text-zinc-400 ${
-                    open === 1 && "rotate-180"
-                  }`}
-                />
-              </div>
-              <AccordionBody>
-                We&apos;re not always in the position that we want to be at.
-                We&apos;re constantly growing. We&apos;re constantly making
-                mistakes. We&apos;re constantly trying to express ourselves and
-                mistakes. We&apos;re constantly trying to express ourselves and
-                mistakes. We&apos;re constantly trying to express ourselves and
-                actualize our dreams.
-              </AccordionBody>
-            </Accordion>
-            <Accordion
-              open={open === 1}
-              className="w-full rounded-lg bg-zinc-200 px-4 py-6 text-sm font-normal md:w-[70%] lg:w-[50%]"
-            >
-              <div
-                onClick={() => handleOpen(1)}
-                className={`group flex w-full max-w-full cursor-pointer items-center justify-between`}
-              >
-                <span className="font-normal text-secondary-black first-letter:uppercase">
-                  tes
-                </span>
-                <ChevronDownIcon
-                  className={`h-4 w-4 text-zinc-700 group-hover:text-zinc-400 ${
-                    open === 1 && "rotate-180"
-                  }`}
-                />
-              </div>
-              <AccordionBody>
-                We&apos;re not always in the position that we want to be at.
-                We&apos;re constantly growing. We&apos;re constantly making
-                mistakes. We&apos;re constantly trying to express ourselves and
-                mistakes. We&apos;re constantly trying to express ourselves and
-                mistakes. We&apos;re constantly trying to express ourselves and
-                actualize our dreams.
-              </AccordionBody>
-            </Accordion>
-            <Accordion
-              open={open === 1}
-              className="w-full rounded-lg bg-zinc-200 px-4 py-6 text-sm font-normal md:w-[70%] lg:w-[50%]"
-            >
-              <div
-                onClick={() => handleOpen(1)}
-                className={`group flex w-full max-w-full cursor-pointer items-center justify-between`}
-              >
-                <span className="font-normal text-secondary-black first-letter:uppercase">
-                  tes
-                </span>
-                <ChevronDownIcon
-                  className={`h-4 w-4 text-zinc-700 group-hover:text-zinc-400 ${
-                    open === 1 && "rotate-180"
-                  }`}
-                />
-              </div>
-              <AccordionBody>
-                We&apos;re not always in the position that we want to be at.
-                We&apos;re constantly growing. We&apos;re constantly making
-                mistakes. We&apos;re constantly trying to express ourselves and
-                mistakes. We&apos;re constantly trying to express ourselves and
-                mistakes. We&apos;re constantly trying to express ourselves and
-                actualize our dreams.
-              </AccordionBody>
-            </Accordion>
+                <div
+                  onClick={() => {
+                    handleClick(i);
+                  }}
+                  className={`group flex w-full max-w-full cursor-pointer items-center justify-between`}
+                >
+                  <span className="font-normal text-secondary-black first-letter:uppercase">
+                    {data.title}
+                  </span>
+                  <ChevronDownIcon
+                    className={`h-4 w-4 text-zinc-700 group-hover:text-zinc-400 ${
+                      open(i) && "rotate-180"
+                    }`}
+                  />
+                </div>
+                {data.list.map((sublist, d) => (
+                  // Elemnt List
+                  <AccordionBody
+                    className={`
+                } w-full bg-zinc-200`}
+                    key={`${sublist.title}-${d}`}
+                  >
+                    <div className="group flex cursor-pointer items-center justify-between px-6 py-2 text-sm font-normal text-secondary-black transition-all delay-0 duration-[0.1s] ease-linear hover:bg-secondary-black">
+                      <span className="group-hover:text-white">
+                        {sublist.title}
+                      </span>
+                      {sublist.access && (
+                        <IconPlay className="h-5 w-5 group-hover:text-primary-hover" />
+                      )}
+                      {!sublist.access && (
+                        <IconLock className="h-5 w-5 group-hover:text-red-400" />
+                      )}
+                    </div>
+                  </AccordionBody>
+                ))}
+              </Accordion>
+            ))}
           </div>
         </div>
       </div>
